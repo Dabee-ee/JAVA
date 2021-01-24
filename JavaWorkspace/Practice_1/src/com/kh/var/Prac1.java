e73832834b32be3418aaf04158a258cb67511530
dabee-ee <movie2529@naver.com>
2021-01-24 22:04:43 +0900

    init

---
+++ JavaWorkspace/Practice_1/src/com/kh/var/Prac1.java
@@ -0,0 +1,34 @@
+package com.kh.var;
+
+public class Prac1 {
+	
+	public static void method1() {
+	
+		String name = "노다비";
+		char sex = 'F';
+		int PassportNo = 456789;
+		short  CustomerNumber = 123;
+		long TicketNumber = 922337203 ;
+		byte GateNumber = 33;
+		float BaggageWeight = 8.9f;
+		boolean AditionalFee = true;	
+		double detailedFee= 3.65;
+
+	
+
+		
+		System.out.println(name);
+		System.out.println(sex);
+		System.out.println(PassportNo);
+		System.out.println(CustomerNumber);
+		System.out.println(TicketNumber);
+		System.out.println(GateNumber);
+		System.out.println(BaggageWeight);
+		System.out.println(AditionalFee);
+		System.out.println(detailedFee);
+		
+	}
+	
+	
+
+}
